39a8ad9afc568893984273b327bb806c2f99fc4c
Nick Todd <nick.todd@conygre.com>
2021-07-08 16:20:03 +0100

    Added support for Java 11 and added a 404

--- Solutions/workspace/CompactDiscDaoWithRestAndBoot/src/main/java/com/conygre/spring/boot/AppConfig.java
+++ Solutions/workspace/CompactDiscDaoWithRestAndBoot/src/main/java/com/conygre/spring/boot/AppConfig.java
@@ -2,6 +2,7 @@ package com.conygre.spring.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,9 +16,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableAutoConfiguration()
-//@EnableJpaRepositories
-@ComponentScan
+@SpringBootApplication
 @Import(SwaggerConfig.class)
 public class AppConfig {
 	    public static void main(String[] args) {
